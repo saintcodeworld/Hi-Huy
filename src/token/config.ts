b132aec2e40c00dfdb8d7ff66f0698e58b6beec0
saintcodeworld <sainttwww@gmail.com>
2026-02-13 22:22:11 +0400

    feat: add core token metadata and configuration module

---
+++ src/token/config.ts
@@ -0,0 +1,15 @@
+export const TOKEN_NAME = 'Hi Huy';
+export const TOKEN_SYMBOL = '$HI HUY TOK';
+export const TOKEN_DECIMALS = 9;
+export const TOTAL_SUPPLY = 1000000000;
+export const MINT_ADDRESS = '{{MINT_ADDRESS}}';
+export const NETWORK = 'mainnet-beta';
+
+export const DEX_LINKS = {
+  pumpfun: `https://pump.fun/${MINT_ADDRESS}`,
+  jupiter: `https://jup.ag/swap/SOL-${MINT_ADDRESS}`,
+  raydium: `https://raydium.io/swap/?inputMint=sol&outputMint=${MINT_ADDRESS}`,
+  birdeye: `https://birdeye.so/token/${MINT_ADDRESS}?chain=solana`,
+  dexscreener: `https://dexscreener.com/solana/${MINT_ADDRESS}`,
+  solscan: `https://solscan.io/token/${MINT_ADDRESS}`
+};
